cd476b23304936000bbaf38f0766609584bec27f
juliafan55 <juliafan55@gmail.com>
2022-06-03 14:23:11 -0400

    install dependencies

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-//require express
-const express = require("express")
-const app = express()
+//dependencies
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config()
 
 app.listen(process.env.PORT,() => console.log("Backend is running"))

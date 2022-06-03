fafc1e6c205221249b3728f4ef918d166469b281
juliafan55 <juliafan55@gmail.com>
2022-06-03 14:58:20 -0400

    added user route

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const helmet = require("helmet");
 
-dotenv.config()
+const userRoute = require("./controllers/users")
+const authRoute = require("./controllers/auth")
 
 //connect to db
 require('./config/db.connection');
+dotenv.config();
 
+//middleware
+app.use(express.json());
+app.use(morgan("common"));
+app.use(helmet());
+
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+
+
+//listen route
 app.listen(process.env.PORT,() => console.log("Backend is running"))

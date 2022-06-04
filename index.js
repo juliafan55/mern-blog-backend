93d3f5d2b0bbd6b2ad299f8950bbd0b6bb1deda5
juliafan55 <juliafan55@gmail.com>
2022-06-04 18:30:43 -0400

    passport auth complete

--- index.js
+++ index.js
@@ -1,48 +1,79 @@
 //dependencies
+const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-const logger = require("morgan")
-const mongoose = require("mongoose");
-const cors = require("cors");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session)
+//models
+const User = require('./models/User')
 
-const passport = require("./Auth")
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-require("dotenv").config();
-
-//connect to db
+//connection to db
 require('./config/db.connection');
 
-//auth
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}))
+
+app.use(cookieParser(process.env.SECRET))
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+}))
+
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(
-    session({
-        resave: false,
-        saveUninitialized: false,
-        secret: process.env.mongoDB_secret,
-        store: new MongoStore({
-            mongooseConnection: mongoose.connection,
-        })
-    })
-)
+require("./passportConfig")(passport);
 
-//middleware
-app.use(logger("dev"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+//routes
+//login
+app.post("/login", (req, res, next) => {
+    passport.authenticate("local", (err, user, info) => {
+        if (err) throw err;
+        if (!user) res.send("No user found")
+        else {
+            req.login(user, err => {
+                if (err) throw err;
+                res.send("Successfully authenticated");
+                console.log(req.user);
+            })
+        }
+    })(req, res, next);
+})
 
+//register
+app.post("/register", (req, res) => {
+    User.findOne({ username: req.body.username }, async (err, doc) => {
+        if (err) throw err;
+        if (doc) res.send("User already exists");
+        if (!doc) {
+            const hashedPassword = await bcrypt.hash(req.body.password, 12);
 
-//Using routers
-const Authentication = require("./routes/Auth");
-const User = require("./routes/User");
+            const newUser = new User({
+                username: req.body.username,
+                password: hashedPassword,
+            })
+            await newUser.save();
+            res.send("User created")
+        }
+    });
+})
 
-app.use("/auth", Authentication);
-app.use("/user", User);
+//user
+app.get("/user", (req, res) => {
+    res.send(req.user);
+})
 
-//defining port for server
-app.listen(process.env.PORT,() => console.log("Backend is running"))
+app.listen(process.env.PORT, () => console.log("Backend is running"))
--- models/User.js
+++ models/User.js
@@ -1,46 +1,8 @@
 const mongoose = require("mongoose")
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, "Username cannot be empty"],
-        unique: true,
-        min: 3,
-        max: 20,
-    },
-    password: {
-        type: String,
-        required: [true, "Password cannot be empty"],
-        min: [6, "Password must be at least 6 digits long"],
-    },
-    email: {
-        type: String,
-        required: [true, "Email cannot be empty"],
-        unique: true,
-        max: 50,
-    },
-    profilePicture: {
-        type: String,
-        default: "",
-    },
-    coverPicture: {
-        type: String,
-        default: "",
-    },
-    followers: {
-        type: Array,
-        default: [],
-    },
-    following: {
-        type: Array,
-        default: [],
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
-},
-    { timestamps: true }
-);
+    username: String,
+    password: String,
+});
 
 module.exports = mongoose.model("User", UserSchema);

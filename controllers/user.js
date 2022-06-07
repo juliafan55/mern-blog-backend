f4ac31b9df55e59678c6a7e5eb00e7df0de49dac
juliafan55 <juliafan55@gmail.com>
2022-06-07 10:08:21 -0400

    delete user

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,11 @@
+const _ = require("lodash")
 const User = require("../models/User");
 
 //find user
 exports.userById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
         if (err || !user) {
-            return res.status(400).json({error: "User not found"})
+            return res.status(400).json({error: "User not found."})
         }
         //add profile object in req with user info
         req.profile = user
@@ -16,7 +17,7 @@ exports.userById = (req, res, next, id) => {
 exports.hasAuthorization = (req, res, next) => {
     const authorized = req.profile && req.auth && req.profile._id === req.profile._id
     if (!authorized) {
-        return res.status(403).json({ error: "User is not authorized to perform this action" });
+        return res.status(403).json({ error: "User is not authorized to perform this action." });
     }
 }
 
@@ -27,5 +28,37 @@ exports.allUsers = (req, res) => {
             return res.statis(400).json({ error: err });
         }
         res.json({ users: users })
-    }).select("username email updated created")
+    }).select("username email updated created.")
+}
+
+exports.getUser = (req, res) => {
+    //remove password from the return
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    
+    return res.json(req.profile);
+}
+
+exports.updateUser = (req, res, next) => {
+    let user = req.profile
+    user = _.extend(user, req.body) //lodash library - mutate the source object
+    user.updated = Date.now()
+    user.save((err) => {
+        if (err) {
+            return res.status(400).json({ error: "Not authorized." });
+        }
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json({ user });
+    })
+}
+
+exports.deleteUser = (req, res, next) => {
+    let user = req.profile
+    user.remove((err, user) => {
+        if (err) {
+            return res.status(400).json({ error: err });
+        }
+        res.json({ message: "User has been deleted." });
+    })
 }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { userById, allUsers } = require("../controllers/user");
+const { requireSignin } = require("../controllers/auth")
+const { userById, allUsers, getUser, updateUser, deleteUser } = require("../controllers/user");
 
 router.get('/users', allUsers);
+router.get('/user/:userId', requireSignin, getUser);
+router.put('/user/:userId', requireSignin, updateUser);
+router.delete('/user/:userId', requireSignin, deleteUser);
 
 //if there's userId, userById method will run
 router.param("userId", userById)
